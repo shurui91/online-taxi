4a2a61e1954700e03c6ba19a75b78840ace148d9
Aaron Liu <shurui91@gmail.com>
2024-04-10 09:19:05 -0700

    first commit

---
+++ api-passenger/src/main/java/com/msb/apipassenger/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.msb.apipassenger.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "test success from passenger";
+    }
+}
