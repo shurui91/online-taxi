49b78dbf27c8794e6984c9a4a2a81d88bf9a5226
Aaron Liu <shurui91@gmail.com>
2024-04-11 11:47:13 -0700

    redis verification function is done

--- api-passenger/src/main/java/com/msb/apipassenger/service/VerificationCodeService.java
+++ api-passenger/src/main/java/com/msb/apipassenger/service/VerificationCodeService.java
@@ -1,9 +1,11 @@
 package com.msb.apipassenger.service;
 
 import com.msb.apipassenger.remote.ServiceVerificationcodeClient;
+import com.msb.internalcommon.constant.CommonStatusEnum;
 import com.msb.internalcommon.dto.ResponseResult;
 import com.msb.internalcommon.response.NumberCodeResponse;
 import com.msb.internalcommon.response.TokenResponse;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -38,11 +40,20 @@ public class VerificationCodeService {
         // 存入redis
         System.out.println("将手机号和验证码存入redis");
         // key, value, 过期时间
-        String key = verificationCodePrefix + phoneNumber;
+        String key = generateKeyByPhone(phoneNumber);
         // 存入redis
         redisTemplate.opsForValue().set(key, numberCode, 2, TimeUnit.MINUTES);
         // 通过电信服务商，发送验证码到手机，这里不实现
         return ResponseResult.success();
+    }
+
+    /**
+     * 根据手机号生成key
+     * @param phoneNumber
+     * @return
+     */
+    private String generateKeyByPhone(String phoneNumber) {
+        return verificationCodePrefix + phoneNumber;
     }
 
     /**
@@ -54,7 +65,23 @@ public class VerificationCodeService {
     public ResponseResult checkCode(String passengerPhone, String verificationCode) {
         // 根据手机号，去redis读取验证码
         System.out.println("根据手机号，去redis读取验证码");
+        // 生成key
+        String key = generateKeyByPhone(passengerPhone);
+        // 根据key获取value
+        Integer codeInRedis = (Integer) redisTemplate.opsForValue().get(key);
+        System.out.println("redis中的验证码为：" + codeInRedis);
+
         // 校验验证码
+        // 判断验证码是否为空
+        if (StringUtils.isBlank(codeInRedis + "") || codeInRedis == null) {
+            return ResponseResult.fail(CommonStatusEnum.VERIFICATION_CODE_ERROR.getCode(),
+                    CommonStatusEnum.VERIFICATION_CODE_ERROR.getValue());
+        }
+        // 判断验证码是否正确
+        if (!codeInRedis.equals(Integer.parseInt(verificationCode))) {
+            return ResponseResult.fail(CommonStatusEnum.VERIFICATION_CODE_ERROR.getCode(),
+                    CommonStatusEnum.VERIFICATION_CODE_ERROR.getValue());
+        }
         System.out.println("校验验证码");
         // 判断原来是否有用户，并进行对应处理
         System.out.println("判断原来是否有用户，并进行对应处理");
--- internal-common/src/main/java/com/msb/internalcommon/constant/CommonStatusEnum.java
+++ internal-common/src/main/java/com/msb/internalcommon/constant/CommonStatusEnum.java
@@ -1,6 +1,10 @@
 package com.msb.internalcommon.constant;
 
 public enum CommonStatusEnum {
+    /**
+     * 验证码错误提示：1000-1099
+     */
+    VERIFICATION_CODE_ERROR(1099, "验证码不正确"),
     SUCCESS(1, "success"),
     FAIL(0, "fail");
     int code;
