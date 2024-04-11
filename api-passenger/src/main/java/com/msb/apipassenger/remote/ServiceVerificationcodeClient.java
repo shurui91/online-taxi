689f27c5e68a863e22e17f6062d92b4b38c6b50c
Aaron Liu <shurui91@gmail.com>
2024-04-10 21:38:32 -0700

    we can get the verification code now

---
+++ api-passenger/src/main/java/com/msb/apipassenger/remote/ServiceVerificationcodeClient.java
@@ -0,0 +1,14 @@
+package com.msb.apipassenger.remote;
+
+import com.msb.internalcommon.dto.ResponseResult;
+import com.msb.internalcommon.response.NumberCodeResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient("service-verificationcode")
+public interface ServiceVerificationcodeClient {
+    @RequestMapping(method = RequestMethod.GET, value = "/numberCode/{size}")
+    ResponseResult<NumberCodeResponse> getNumberCode(@PathVariable("size") int size);
+}
--- api-passenger/src/main/java/com/msb/apipassenger/service/VerificationCodeService.java
+++ api-passenger/src/main/java/com/msb/apipassenger/service/VerificationCodeService.java
@@ -1,14 +1,26 @@
 package com.msb.apipassenger.service;
 
+import com.msb.apipassenger.remote.ServiceVerificationcodeClient;
+import com.msb.internalcommon.dto.ResponseResult;
+import com.msb.internalcommon.response.NumberCodeResponse;
 import net.sf.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VerificationCodeService {
+    @Autowired
+    private ServiceVerificationcodeClient serviceVerificationcodeClient;
+
     public String getVerificationCode(String phoneNumber) {
         // 调用验证码服务，获取验证码
         System.out.println("调用验证码服务，为手机号" + phoneNumber + "发送验证码");
-        String code = "666666";
+        ResponseResult<NumberCodeResponse> numberCodeResponse =
+                serviceVerificationcodeClient.getNumberCode(6);
+        int numberCode = numberCodeResponse.getData().getNumberCode();
+        System.out.println("获取到的验证码为：" + numberCode);
+
+
         // 存入redis
         System.out.println("将手机号和验证码存入redis");
         // 返回验证码
