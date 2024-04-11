9b479ad5ef7325d08a7bd3b8128cca4afd7a211b
Aaron Liu <shurui91@gmail.com>
2024-04-11 09:09:03 -0700

    review feign client code

--- api-passenger/src/main/java/com/msb/apipassenger/remote/ServiceVerificationcodeClient.java
+++ api-passenger/src/main/java/com/msb/apipassenger/remote/ServiceVerificationcodeClient.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+// 使用注解@FeignClient 定义feign客户端
+// value = "service-verificationcode" 指定服务提供者的应用名称
 @FeignClient("service-verificationcode")
 public interface ServiceVerificationcodeClient {
     @RequestMapping(method = RequestMethod.GET, value = "/numberCode/{size}")
