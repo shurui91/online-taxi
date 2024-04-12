87fea444dc8272df9a7db1a95933c1d954c79c22
Aaron Liu <shurui91@gmail.com>
2024-04-12 00:59:19 -0700

    search for user info based on phone number

---
+++ service-passenger-user/src/main/java/com/msb/servicepassengeruser/mapper/PassengerUserMapper.java
@@ -0,0 +1,9 @@
+package com.msb.servicepassengeruser.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.msb.servicepassengeruser.dto.PassengerUser;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PassengerUserMapper extends BaseMapper<PassengerUser> {
+}
--- service-passenger-user/src/main/java/com/msb/servicepassengeruser/service/UserService.java
+++ service-passenger-user/src/main/java/com/msb/servicepassengeruser/service/UserService.java
@@ -1,14 +1,27 @@
 package com.msb.servicepassengeruser.service;
 
 import com.msb.internalcommon.dto.ResponseResult;
+import com.msb.servicepassengeruser.dto.PassengerUser;
+import com.msb.servicepassengeruser.mapper.PassengerUserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class UserService {
+    @Autowired
+    private PassengerUserMapper passengerUserMapper;
+
     public ResponseResult loginOrRegister(String passengerPhone) {
         System.out.println("user service被调用，手机号： " + passengerPhone);
         // 根据手机号查询用户信息
-
+        Map<String, Object> map = new HashMap<>();
+        map.put("passenger_phone", passengerPhone);
+        List<PassengerUser> passengerUsers = passengerUserMapper.selectByMap(map);
+        System.out.println(passengerUsers == null ? "无记录" : passengerUsers.get(0).getPassengerPhone());
         // 判断用户信息是否存在，不存在则插入用户信息
         return ResponseResult.success();
     }
