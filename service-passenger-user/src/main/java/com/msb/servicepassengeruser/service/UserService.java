3d9d44e8318ae87c650879ac32cf2a2e4a84b2c4
Aaron Liu <shurui91@gmail.com>
2024-04-12 10:18:37 -0700

    insert default data when the phone number is not found in db

--- service-passenger-user/src/main/java/com/msb/servicepassengeruser/service/UserService.java
+++ service-passenger-user/src/main/java/com/msb/servicepassengeruser/service/UserService.java
@@ -6,6 +6,7 @@ import com.msb.servicepassengeruser.mapper.PassengerUserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,8 +22,20 @@ public class UserService {
         Map<String, Object> map = new HashMap<>();
         map.put("passenger_phone", passengerPhone);
         List<PassengerUser> passengerUsers = passengerUserMapper.selectByMap(map);
-        System.out.println(passengerUsers == null ? "无记录" : passengerUsers.get(0).getPassengerPhone());
+        // System.out.println(passengerUsers == null ? "无记录" : passengerUsers
+        // .get(0).getPassengerPhone());
         // 判断用户信息是否存在，不存在则插入用户信息
+        if (passengerUsers == null || passengerUsers.size() == 0) {
+            PassengerUser passengerUser = new PassengerUser();
+            passengerUser.setPassengerName("张三");
+            passengerUser.setPassengerGender((byte) 1);
+            passengerUser.setPassengerPhone(passengerPhone);
+            passengerUser.setState((byte) 0);
+            LocalDateTime now = LocalDateTime.now();
+            passengerUser.setGmtCreate(now);
+            passengerUser.setGmtModified(now);
+            passengerUserMapper.insert(passengerUser);
+        }
         return ResponseResult.success();
     }
 }
