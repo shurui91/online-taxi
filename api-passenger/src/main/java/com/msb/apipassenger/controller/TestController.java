7cb06fcd64b497eab5eb4759e605fe3d3e1546bc
Aaron Liu <shurui91@gmail.com>
2024-04-12 18:07:04 -0700

    test token controller

--- api-passenger/src/main/java/com/msb/apipassenger/controller/TestController.java
+++ api-passenger/src/main/java/com/msb/apipassenger/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.msb.apipassenger.controller;
 
+import com.msb.internalcommon.dto.ResponseResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,23 @@ public class TestController {
     @GetMapping("/test")
     public String test() {
         return "test success from passenger";
+    }
+
+    /**
+     * 需要有token
+     * @return
+     */
+    @GetMapping("/authTest")
+    public ResponseResult authTest() {
+        return ResponseResult.success("auth test");
+    }
+
+    /**
+     * 不需要token
+     * @return
+     */
+    @GetMapping("/noAuthTest")
+    public ResponseResult noAuthTest() {
+        return ResponseResult.success("no auth test");
     }
 }
