2a22bf4089e60dd9762da4d894070f7e17777e1b
Aaron Liu <shurui91@gmail.com>
2024-04-17 10:36:49 -0700

    try to parse the phone number from token, it works

--- api-passenger/src/main/java/com/msb/apipassenger/service/UserService.java
+++ api-passenger/src/main/java/com/msb/apipassenger/service/UserService.java
@@ -2,6 +2,8 @@ package com.msb.apipassenger.service;
 
 import com.msb.internalcommon.dto.PassengerUser;
 import com.msb.internalcommon.dto.ResponseResult;
+import com.msb.internalcommon.dto.TokenResult;
+import com.msb.internalcommon.util.JwtUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,9 @@ public class UserService {
     public ResponseResult getUserByAccessToken(String accessToken) {
         log.info("accessToken: {}", accessToken);
         // 解析accessToken，拿到手机号
+        TokenResult tokenResult = JwtUtils.checkToken(accessToken);
+        String phone = tokenResult.getPhone();
+        log.info("phone: {}", phone);
 
         // 根据手机号查询用户信息
         PassengerUser passengerUser = new PassengerUser();
