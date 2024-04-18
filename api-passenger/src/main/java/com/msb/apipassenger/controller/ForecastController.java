646f7d3497adc5cf7dc024857708fcd690b41e99
Aaron Liu <shurui91@gmail.com>
2024-04-18 14:30:10 -0700

    forecast price function, can get the dummy data now

---
+++ api-passenger/src/main/java/com/msb/apipassenger/controller/ForecastController.java
@@ -0,0 +1,24 @@
+package com.msb.apipassenger.controller;
+
+import com.msb.apipassenger.service.ForecastPriceService;
+import com.msb.internalcommon.dto.ResponseResult;
+import com.msb.internalcommon.request.ForecastPriceDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ForecastController {
+    @Autowired
+    private ForecastPriceService forecastPriceService;
+
+    @PostMapping("/forecast-price")
+    public ResponseResult forecastPrice(@RequestBody ForecastPriceDTO forecastPriceDTO) {
+        String depLongitude = forecastPriceDTO.getDepLongitude();
+        String depLatitude = forecastPriceDTO.getDepLatitude();
+        String destLongitude = forecastPriceDTO.getDestLongitude();
+        String destLatitude = forecastPriceDTO.getDestLatitude();
+        return forecastPriceService.forcastPrice(depLongitude, depLatitude, destLongitude, destLatitude);
+    }
+}
