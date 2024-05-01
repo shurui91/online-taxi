fe951c66fb2cf187c87d1b92aaafa4c3476bff97
Aaron Liu <shurui91@gmail.com>
2024-05-01 14:24:18 -0700

    api-boss is registered to nacos

---
+++ api-boss/src/main/java/com/msb/apiBoss/ApiBossApplication.java
@@ -0,0 +1,11 @@
+package com.msb.apiBoss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBossApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiBossApplication.class, args);
+    }
+}
