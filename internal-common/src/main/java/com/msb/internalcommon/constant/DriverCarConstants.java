d0240dd21e1230581646cf6f3b42d7e635de38c6
Aaron Liu <shurui91@gmail.com>
2024-05-03 10:28:29 -0700

    car-driver unbind works

--- internal-common/src/main/java/com/msb/internalcommon/constant/DriverCarConstants.java
+++ internal-common/src/main/java/com/msb/internalcommon/constant/DriverCarConstants.java
@@ -4,9 +4,9 @@ public class DriverCarConstants {
     /**
      * 司机车辆关系状态：绑定
      */
-    public static int DRIVER_CAR_BIND = 1;
+    public static Integer DRIVER_CAR_BIND = 1;
     /**
      * 司机车辆关系状态：解绑
      */
-    public static int DRIVER_CAR_UNBIND = 2;
+    public static Integer DRIVER_CAR_UNBIND = 2;
 }
--- service-driver-user/src/main/java/com/msb/serviceDriverUser/controller/DriverCarBindingRelationshipController.java
+++ service-driver-user/src/main/java/com/msb/serviceDriverUser/controller/DriverCarBindingRelationshipController.java
@@ -29,4 +29,9 @@ public class DriverCarBindingRelationshipController {
     public ResponseResult bind(@RequestBody DriverCarBindingRelationship driverCarBindingRelationship) {
         return driverCarBindingRelationshipService.bind(driverCarBindingRelationship);
     }
+
+    @PostMapping("/unbind")
+    public ResponseResult unbind(@RequestBody DriverCarBindingRelationship driverCarBindingRelationship) {
+        return driverCarBindingRelationshipService.unbind(driverCarBindingRelationship);
+    }
 }
