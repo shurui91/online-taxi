ccb8bc8a2e82534d877020edd597cdce40a91466
Aaron Liu <shurui91@gmail.com>
2024-05-02 19:05:16 -0700

    api-driver service created

---
+++ api-driver/src/main/java/com/msb/apiDriver/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.msb.apiDriver.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "api-driver is running";
+    }
+}
